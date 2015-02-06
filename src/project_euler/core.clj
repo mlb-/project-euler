1f799067fc21408f0f09e11ed6cec49c26c4138c
Matthew Batema <MBatema@trustwave.com>
2015-02-06 15:19:06 -0800

    Solve problem 2.

--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -11,4 +11,20 @@
        set
        (apply +)))
 
+(defn- fibonacci
+  "Provide a Clojure lazy-sequence for Fibonacci numbers."
+  ([]
+   (fibonacci 1 2))
+  ([a b]
+  (cons a (lazy-seq (fibonacci b (+ b a))))))
+
+(defn euler-2
+  "By considering the terms in the Fibonacci sequence whose values do not
+  exceed four million, find the sum of the even-valued terms."
+  [limit]
+  (->> (fibonacci)
+       (take-while (partial > limit))
+       (filter even?)
+       (apply +)))
+
 ; vim: fdm=indent
--- test/project_euler/core_test.clj
+++ test/project_euler/core_test.clj
@@ -1,6 +1,8 @@
 (ns project-euler.core-test
   (:require [clojure.test :refer :all]
-            [project-euler.core :refer :all]))
+            [project-euler.core
+             :as impl
+             :refer :all]))
 
 (deftest euler-1-test
   (testing "Example problem"
@@ -9,5 +11,16 @@
   (testing "Actual problem"
     (is (= (euler-1 1000 3 5)
            233168))))
+
+(deftest euler-2-test
+  (testing "Fibonacci"
+    (is (= (take 10 (#'impl/fibonacci))
+           [1 2 3 5 8 13 21 34 55 89])))
+  (testing "Test problem"
+    (is (= (euler-2 89)
+           44)))
+  (testing "Actual problem"
+    (is (= (euler-2 (int 4e6))
+           4613732))))
 
 ; vim: fdm=indent
